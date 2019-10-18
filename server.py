8f0ed2196ef46a6948b7bbd792535f56adb4d5da
arkhchance <arkhchance@protonmail.com>
2019-10-18 09:43:10 +0200

    rewrite server proprely

--- server.py
+++ server.py
@@ -1,69 +1,93 @@
 #!/usr/bin/python
-import socket
-import sys
-from threading import Thread
+import socket, sys, threading
+import signal, time
 
-import socket, select
+#change as needed
+HOST = '0.0.0.0'
+PORT = 44988
+BUFFER = 1024
 
-#Function to broadcast chat messages to all connected clients
-def broadcast_data (sock, message):
-	#Do not send the message to master socket and the client who has send us the message
-	for socket in CONNECTION_LIST:
-		if socket != server_socket :
-			try :
-				socket.sendall(message.encode())
-			except :
-				# broken socket connection may be, chat client pressed ctrl+c for example
-				socket.close()
-				CONNECTION_LIST.remove(socket)
+#don't touch !
+run = True
+conn_client = {}                # active connection array
+
+def receiveSignal(signalNumber, frame):
+    print('Received Signal ', signalNumber)
+    print("exiting..")
+    run = False
+    time.sleep(2)
+    for con in conn_client:
+        try:
+            conn_client[con].close()
+        except socket.error as e:
+            print("error sending ",str(e))
+    sys.exit()
+    return
+
+class ThreadClient(threading.Thread):
+
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.connexion = conn
+
+    def run(self):
+        nom = self.getName()        # thread name
+        while run:
+            msgClient = self.connexion.recv(BUFFER)
+
+            #if client send QUIT => terminate the conenction
+            if msgClient.decode().upper() == "QUIT" :
+                break
+
+            message = msgClient.decode()
+            print("received ", message)
+            # send to all client :
+            for cle in conn_client:
+                try:
+                    conn_client[cle].send(message.encode())
+                except socket.error as e:
+                    print("error sending ",str(e))
+
+        # close connection :
+        self.connexion.close()
+        del conn_client[nom]        # del from array
+        print("Client disconnected")
+        # end function
+
+def main() :
+
+    signal.signal(signal.SIGTERM, receiveSignal)
+    signal.signal(signal.SIGQUIT, receiveSignal)
+    signal.signal(signal.SIGINT, receiveSignal)
+
+    # server init
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        mySocket.bind((HOST, PORT))
+    except socket.error as e:
+        print("Can't bind quitting. ", str(e))
+        sys.exit()
+
+    print("Server is ready.. ")
+    mySocket.listen(10)
+
+
+    while run:
+        connexion, adresse = mySocket.accept()
+        # new thread :
+        th = ThreadClient(connexion)
+        th.start()
+
+        it = th.getName()        # thread id
+        conn_client[it] = connexion
+        print ("Client %s connected, IP address %s, port %s." %(it, adresse[0], adresse[1]))
+        # Answer to connection sucess :
+        msg = "Connected"
+        connexion.send(msg.encode())
+
+
 
 if __name__ == "__main__":
-
-	# List to keep track of socket descriptors
-	CONNECTION_LIST = []
-	RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-	PORT = 44988
-
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# this has no effect, why ?
-	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	server_socket.bind(("0.0.0.0", PORT))
-	server_socket.listen(10)
-
-	# Add server socket to the list of readable connections
-	CONNECTION_LIST.append(server_socket)
-
-	print("started on port " + str(PORT))
-
-	while 1:
-		# Get the list sockets which are ready to be read through select
-		read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
-
-		for sock in read_sockets:
-			#New connection
-			if sock == server_socket:
-				# Handle the case in which there is a new connection recieved through server_socket
-				sockfd, addr = server_socket.accept()
-				CONNECTION_LIST.append(sockfd)
-				print("Client (%s, %s) connected" % addr)
-
-				broadcast_data(sockfd, "New Player" )
-
-			#Some incoming message from a client
-			else:
-				# Data recieved from client, process it
-				try:
-					#In Windows, sometimes when a TCP program closes abruptly,
-					# a "Connection reset by peer" exception will be thrown
-					data = sock.recv(RECV_BUFFER)
-					if data:
-						broadcast_data(sock,data.decode())
-
-				except:
-					broadcast_data(sock, "Client (%s, %s) is offline" % addr)
-					print("Client (%s, %s) is offline" % addr)
-					sock.close()
-					CONNECTION_LIST.remove(sock)
-					continue
-
-	server_socket.close()
+    # execute only if run as a script
+    main()
