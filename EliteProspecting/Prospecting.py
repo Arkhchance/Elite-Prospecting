fcb3b763ca14711a78c2ce5961cb9a00a4846b97
arkhchance <arkhchance@protonmail.com>
2019-10-19 09:11:18 +0200

    g

--- EliteProspecting/Prospecting.py
+++ EliteProspecting/Prospecting.py
@@ -11,6 +11,9 @@ class Prospecting():
         self.parent = None
         self.total_msg  = 0
         self.messages = []
+        self.colors = []
+        self.total_msg_display = 6
+        self.status = [None] * self.total_msg_display
         self.buffer = 1024
         self.sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
         self.load_config()
@@ -20,15 +23,15 @@ class Prospecting():
         self.ip = config.get("server_ip") or "127.0.0.1"
         self.port = config.get("server_port") or 44988
 
-        self.track_LTD = config.getint("track_LTD") or 1
-        self.track_Painite = config.getint("track_Painite") or 1
-        self.new_win = config.getint("use_new_window") or 0
+        self.track_LTD = config.getint("track_LTD")
+        self.track_Painite = config.getint("track_Painite")
+        self.new_win = config.getint("use_new_window")
+        self.win_trans = config.getint("font_size")
+        self.miss = config.getint("miss")
 
         self.ltd_threshold = config.get("LTD_t") or 18
         self.painite_threshold = config.get("Painite_t") or 25
         self.font_size = config.get("font_size") or 14
-        print("new win ", self.new_win)
-        print("ltd ", self.track_LTD)
 
     def init_gui(self,parent):
         self.parent = parent
@@ -39,10 +42,11 @@ class Prospecting():
         self.connection = tk.Button(self.frame, text="Connect to server", command=self.connect)
         self.connection.grid(row=row, columnspan=2)
         if self.new_win == 0:
-            self.status = tk.Label(self.frame, text="", foreground="yellow")
-            self.status.config(font=("Courier", int(self.font_size)))
-            row += 1
-            self.status.grid(row=row, pady=5, sticky=tk.W)
+            for i in range(self.total_msg_display):
+                self.status[i] = tk.Label(self.frame, text="", foreground="yellow")
+                self.status[i].config(font=("Courier", int(self.font_size)))
+                row += 1
+                self.status[i].grid(row=row, pady=5, sticky=tk.W)
         else :
             self.win_x = tk.Scale(self.frame, from_=1, to=6000, orient=tk.HORIZONTAL, label="x position",command=self.update_new_win)
             self.win_x.grid(row=row+3, columnspan=2)
@@ -53,26 +57,38 @@ class Prospecting():
             self.win_y.set(100)
 
             self.window = tk.Toplevel()
+            if sys.platform == 'win32' and self.win_trans == 1 :
+                self.window.attributes("-transparentcolor", 'black')
             self.window.attributes("-alpha", 0.75)
             self.window.wm_attributes("-topmost", True)
             self.window.overrideredirect(True)
+            self.window.configure(background='black')
             self.window.wm_geometry('+' + str(439) + '+' + str(172))
-            self.status = tk.Label(self.window, text="Waiting..",foreground="yellow")
-            self.status.config(font=("Courier", int(self.font_size)))
-            self.status.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+            for i in range(self.total_msg_display):
+                self.status[i] = tk.Label(self.window, text="Waiting..",foreground="red")
+                self.status[i].config(font=("Courier", int(self.font_size)),background='black')
+                self.status[i].pack(side="top", fill="both", expand=True, padx=10, pady=10)
 
         return self.frame
 
     def update_new_win(self,val):
         self.window.wm_geometry('+' + str(self.win_x.get()) + '+' + str(self.win_y.get()))
 
-    def display_msg(self,msg):
-        msg = msg + "\n"
-        val = ""
+    def display_msg(self,msg,mine=True):
+        #msg = msg + "\n"
+        #val = ""
+        if mine :
+            color = "Yellow"
+        else :
+            color = "red"
+
+        self.colors.append(color)
+        self.messages.append(msg)
         self.total_msg += 1
 
-        if self.total_msg >= 7 :
+        if self.total_msg > self.total_msg_display :
             self.messages.pop(0)
+            self.colors.pop(0)
             self.total_msg -= 1
 
         self.messages.append(msg)
@@ -130,7 +146,7 @@ class Prospecting():
                 msg = self.recvMsg()
                 if msg.decode() == "quit":
                     break
-                self.display_msg(msg)
+                self.display_msg(msg,False)
             except socket.error as e:
                 print("error receiving")
                 print("Caught exception socket.error : %s" % e)
@@ -140,7 +156,7 @@ class Prospecting():
         message = message.format(prop)
         if self.connected :
             self.sendMsg(message)
-        self.display_msg(message)
+        self.display_msg(message,True)
 
     def event(self,cmdr,entry):
         #received a ProspectedAsteroid event
@@ -149,6 +165,12 @@ class Prospecting():
             if mat['Name'] == "LowTemperatureDiamond" and self.track_LTD == 1 :
                 if mat['Proportion'] > float(self.ltd_threshold):
                     self.publish(cmdr,mat['Name_Localised'],mat['Proportion'])
+                elif self.miss == 1 :
+                    self.display_msg("Asteroid below threshold",True)
             elif mat['Name'] == "Painite" and self.track_Painite == 1 :
                 if mat['Proportion'] > float(self.painite_threshold):
                     self.publish(cmdr,mat['Name_Localised'],mat['Proportion'])
+                elif self.miss == 1 :
+                    self.display_msg("Asteroid below threshold",True)
+            elif self.miss == 1 :
+                self.display_msg("Wrong Asteroid",True)
--- EliteProspecting/load.py
+++ EliteProspecting/load.py
@@ -12,39 +12,50 @@ def plugin_prefs(parent,cmdr,is_beta):
     PADX = 10
     BUTTONX = 12	# indent Checkbuttons and Radiobuttons
     PADY = 2		# close spacing
-
+    row = 1
     frame = nb.Frame(parent)
     frame.columnconfigure(1, weight=1)
-    nb.Label(frame,text="Configuration").grid()
+
 
     this.ltd_p = tk.IntVar(value=config.getint("track_LTD") and 1)
     this.painite_p = tk.IntVar(value=config.getint("track_Painite") and 1)
     this.new_win = tk.IntVar(value=config.getint("use_new_window") and 1)
+    this.win_trans = tk.IntVar(value=config.getint("win_trans") and 1)
+    this.miss = tk.IntVar(value=config.getint("miss") and 1)
 
     this.ip_label = nb.Label(frame,text="Server IP")
-    this.ip_label.grid(row=3, padx=PADX, sticky=tk.W)
+    this.ip_label.grid(row=row, padx=PADX, sticky=tk.W)
     this.server_ip = nb.Entry(frame)
-    this.server_ip.grid(row=3, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
+    this.server_ip.grid(row=row, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
 
+    row += 1
     this.port_label = nb.Label(frame,text="Server Port")
-    this.port_label.grid(row=4, padx=PADX, sticky=tk.W)
+    this.port_label.grid(row=row, padx=PADX, sticky=tk.W)
     this.server_port = nb.Entry(frame)
-    this.server_port.grid(row=4, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
+    this.server_port.grid(row=row, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
 
+    row += 1
     this.font_size_label = nb.Label(frame,text="font size")
-    this.font_size_label.grid(row=5, padx=PADX, sticky=tk.W)
+    this.font_size_label.grid(row=row, padx=PADX, sticky=tk.W)
     this.font_size = nb.Entry(frame)
-    this.font_size.grid(row=5, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
+    this.font_size.grid(row=row, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
 
-    nb.Checkbutton(frame, text='Display result on new window', variable=this.new_win).grid(row=6, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
+    row += 1
+    nb.Checkbutton(frame, text='Display result on new window (restart edmc)', variable=this.new_win).grid(row=row, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
+    row += 1
+    nb.Checkbutton(frame, text='Make the window transparent (windows only)', variable=this.win_trans).grid(row=row, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
+    row += 1
+    nb.Checkbutton(frame, text='Display message if asteroid target doesn\'t meet requirement', variable=this.miss).grid(row=row, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
 
-    nb.Checkbutton(frame, text='Search for LTD greater than', variable=this.ltd_p).grid(row=9, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
+    row += 1
+    nb.Checkbutton(frame, text='Search for LTD greater than', variable=this.ltd_p).grid(row=row, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
     this.ltd_threshold = nb.Entry(frame)
-    this.ltd_threshold.grid(row=9, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
+    this.ltd_threshold.grid(row=row, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
 
-    nb.Checkbutton(frame, text='Search for Painite greater than', variable=this.painite_p).grid(row=10, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
+    row += 1
+    nb.Checkbutton(frame, text='Search for Painite greater than', variable=this.painite_p).grid(row=row, column=0, padx=PADX, pady=PADY, sticky=tk.EW)
     this.painite_threshold = nb.Entry(frame)
-    this.painite_threshold.grid(row=10, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
+    this.painite_threshold.grid(row=row, column=1, padx=PADX, pady=PADY, sticky=tk.EW)
 
     load_value()
     return frame
@@ -73,6 +84,9 @@ def prefs_changed(cmdr,is_beta) :
     config.set("server_port",this.server_port.get())
     config.set("font_size",this.font_size.get())
     config.set("use_new_window",this.new_win.get())
+    config.set("win_trans",this.win_trans.get())
+    config.set("miss",this.miss.get())
+    print("set ",this.server_port.get())
     prospecting.load_config()
 
 def plugin_start3(plugin_dir):
